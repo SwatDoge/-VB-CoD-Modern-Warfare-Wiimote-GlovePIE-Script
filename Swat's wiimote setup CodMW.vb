055fb9895311d33eee3c2f81323278fcd2829522
SwatDoge <bob.dedebouwer@hotmail.com>
2021-02-23 01:30:02 +0100

    initial

---
+++ Swat's wiimote setup CodMW.vb
@@ -0,0 +1,43 @@
+// Wiimote mouse script for Windows desktop
+// Requires a sensor bar
+
+var.ButtonFreezeTime = 250ms
+var.PointerBump = KeepDown(Pressed(wiimote.A),var.ButtonFreezeTime) or KeepDown(Pressed(wiimote.B),var.ButtonFreezeTime)
+Wiimote.Led1 = true
+
+// Mouse movement
+if wiimote.PointerVisible but not var.PointerBump then
+  mouse.x = wiimote.PointerX
+  mouse.y = wiimote.PointerY
+end if
+
+// Mouse Buttons
+mouse.LeftButton = Wiimote.B and KeepDown(Wiimote.PointerVisible,0.5s)
+mouse.RightButton = Nunchuk1.Z and KeepDown(Wiimote.PointerVisible,0.5s)
+
+// General
+Escape = Wiimote.Home and KeepDown(Wiimote.PointerVisible,0.5s)
+Tab = Wiimote1.One and KeepDown(Wiimote.PointerVisible,0.5s)
+
+// Movement
+Shift = Wiimote.A and KeepDown(Wiimote.PointerVisible,0.5s)
+WASD = nunchuk1.Joy
+Space = Wiimote1.Up and KeepDown(Wiimote.PointerVisible,0.5s)
+Control = Nunchuck.C and KeepDown(Wiimote.PointerVisible,0.5s)
+
+// Weapons
+R = Nunchuk.SwingDown and KeepDown(Wiimote.PointerVisible,0.5s)
+
+
+// Dpad
+F = Wiimote1.Left and KeepDown(Wiimote.PointerVisible,0.5s)
+V = Wiimote1.Stabbing and KeepDown(Wiimote.PointerVisible,0.5s)
+E = Wiimote1.Right and KeepDown(Wiimote.PointerVisible,0.5s)
+Mouse.WheelDown = Wiimote1.Down and KeepDown(Wiimote.PointerVisible,0.5s)
+
+// Voice
+P = Wiimote1.Two and KeepDown(Wiimote.PointerVisible,0.5s)
+
+// Offsensive
+Four = Wiimote1.Minus and KeepDown(Wiimote.PointerVisible,0.5s)
+G = Wiimote1.Plus and KeepDown(Wiimote.PointerVisible,0.5s)
